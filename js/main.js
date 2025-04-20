6b86994495ceee8a15b263f2286351295dcafe9c
leahabates <lbates3@wisc.edu>
2025-04-20 18:43:11 -0400

    Add data join function
    created a data join function and fixed how the river and TRI sites look on the map. I also re adjusted the position of the map

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 //insert code here!
+var attrArray = ["Total_pop", "white", "black", "poverty_rates", "cancer_inc_rate_per_100000"]
+
+var expressed = attrArray[0]
+
 window.onload = setMap();
 
 // set up map
 function setMap(){
 
      //map frame dimensions
-     var width = 960,
-     height = 700;
+     var width = window.innerWidth * 1,
+     height = window.innerHeight * 1 ;
 
  //create new svg container for the map
     var map = d3.select("body")
@@ -17,11 +21,11 @@ function setMap(){
 
  //create Albers equal area conic projection centered on France
     var projection = d3.geoAlbers()
-        .center([3.64, 30.87])
-        .rotate([95.55, 0, 0]) 
-        .parallels([26.55, 90.00])
-        .scale(10000.00)
-        .translate([width / 2, height / 2]);
+        .center([0, 30.2])
+        .rotate([91,2, 0]) 
+        .parallels([29.5, 30.5])
+        .scale(27500)
+        .translate([width / 2 - 400 , height / 2 + 800]);
 
     var path = d3.geoPath()
         .projection(projection);
@@ -43,8 +47,8 @@ function setMap(){
             cancer_parish = data[2],
             river = data[3],
             sites = data[4];
-        
-        //translate to topojson
+        console.log(csvData)
+        //convert topojson to geojson
         var la_county = topojson.feature(county, county.objects.LA_county),
             la_cancer_parish = topojson.feature(cancer_parish, cancer_parish.objects.cancer_alley_parishes);
             la_river = topojson.feature(river, river.objects.LA_river),
@@ -56,6 +60,9 @@ function setMap(){
         console.log(la_river)
         console.log(tri_sites)
 
+        la_cancer_parish.features = joinData(la_cancer_parish.features, csvData);
+        //console.log(la_cancer_parish.features[0].properties); // to verify
+         
         // add  LA counties
         var counties = map.append("path")
             .datum(la_county)
@@ -68,32 +75,57 @@ function setMap(){
             .enter()
             .append("path")
             .attr("class", function(d){
-                return "regions " + d.properties.NAME;
+                return "cancer_parish regions " + d.properties.NAME;
             })
             .attr("d", path)
 
-        // var mississippi = map.selectAll(".mississippi")
-        //     .data(la_river.features)
-        //     .enter()
-        //     .append("path")
-        //     .attr("class", function (d){
-        //         return "mississippi " + d.properties.name;
-        //     })
-        //     .attr("d", path)
+        var mississippi = map.selectAll(".mississippi")
+             .data(la_river.features)
+             .enter()
+             .append("path")
+             .attr("class", function (d){
+                 return "mississippi " + d.properties.name;
+             })
+             .attr("d", path)
+             .attr("fill", "none")
+             .attr("stroke-width", 4)    
 
-        // var tri = map.selectAll(".tri")
-        //     .data(tri_sites.features)
-        //     .enter()
-        //     .append("circles")
-        //     .attr("class", function (d){
-        //         return "tri " + d.properties.FACILIT;
-        //     })
-        //     .attr("r", 3)
-        //     .attr ("transform", function(d){
-        //         var coords = projection(d.geometry.coordinates);
-        //         return "translate (" + coords + ")";
-        //     })
-
-        // console.log(tri)
+        var tri = map.selectAll(".tri")
+            .data(tri_sites.features)
+            .enter()
+            .append("circle")
+            .attr("class", function (d){
+                return "tri " + d.properties.FACILIT;
+            })
+            .attr("r", 3)
+             .attr ("transform", function(d){
+                var coords = projection(d.geometry.coordinates);
+                 return "translate (" + coords + ")";
+            })
     };
+};
+
+function joinData(la_cancer_parish, csvData){
+    //loop through the csv to set each with the geojson region
+    for (var i = 0; i < csvData.length; i++) {
+        var csvRegion = csvData[i] //the current region
+        var csvKey = csvRegion.Parish // primary key
+
+        //loop through geojson to get the correct county
+        for (var a = 0; a < la_cancer_parish.length; a++){
+            var geojsonProps = la_cancer_parish[a].properties;
+            var geojsonKey = geojsonProps.NAME_ALT;
+
+            //the keys match
+            if (geojsonKey == csvKey){
+
+                attrArray.forEach(function(attr){
+                    var val = parseFloat(csvRegion[attr]);
+                    geojsonProps[attr] = val;
+                    
+                });
+            }
+        }
+    }
+    return la_cancer_parish;
 };
